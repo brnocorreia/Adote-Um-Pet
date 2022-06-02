2e5cc1b22fd70837b6005390f35eec4daebc43a9
Bruno Correia <bruninhocorreia2013@gmail.com>
2022-06-02 20:59:45 -0300

    Create Model Pet, start DB

--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=255)
+    breed = models.CharField(blank=False, null=False, max_length=255)
+    story = models.TextField(blank=False, null=False)
+    photo = models.URLField(blank=False, null=False)
